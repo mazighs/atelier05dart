4a2b8c603fa3f7c179f30426004368cb82038f0d
mazighs <barachemazighoo@.com>
2025-11-15 16:10:11 -0500

    Utilise on FormatException catch

--- exo2.dart
+++ exo2.dart
@@ -6,6 +6,8 @@ import 'dart:io';void main() {
     final age = int.parse(saisie!);
     print("Votre age est : $age");
   } on FormatException {
-    
+    print("Erreur de format : veuillez entrer un nombre valide !");
+  } finally {
+    print("Fin d'operation.");
   }
-  }
+}
